7b943003dd190185d39fdbb3b54e3715aa6605ba
malopestorres <marcuslorenlopes@gmail.com>
2023-08-20 21:14:51 -0300

    refactor: refine project structure

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,8 @@
 module.exports = { 
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'subject-min-length': [2, 'always', 5] 
+      'subject-min-length': [2, 'always', 3],
+      'subject-empty': [2, 'never'],
     } 
   };
   
