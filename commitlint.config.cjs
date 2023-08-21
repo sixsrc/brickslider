f65628b2f6452890bbbf10d74decf79a9802ab8f
malopestorres <marcuslorenlopes@gmail.com>
2023-08-21 06:42:14 -0300

    fix: fix project behavior

--- commitlint.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = { 
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-min-length': [2, 'always', 3],
-      'subject-empty': [2, 'never'],
-    } 
-  };
-  
