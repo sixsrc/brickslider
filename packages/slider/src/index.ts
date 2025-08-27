3e33537cc6c013c5ba5660e0fb8fa010f7321050
“malopestorres” <marcuslorenlopes@gmail.com>
2025-08-26 21:07:32 -0300

    alterações dia 26/08/2025 - parte 2

--- packages/slider/src/index.ts
+++ packages/slider/src/index.ts
@@ -20,8 +20,8 @@ const slider2 = new BrickSlider("#slider2", {
 
 const slider3 = new BrickSlider("#slider3", {
   spacing: 20,
-  slidesPerView: 3,
-  slidesPerPage: 3,
+  slidesPerView: 4,
+  slidesPerPage: 5,
   infinite: false
 })
 /*
